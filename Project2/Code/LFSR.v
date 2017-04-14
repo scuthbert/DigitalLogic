f0ae0c70f8824fd6fb0260e96692c6800746f802
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2017-04-14 01:53:03 -0600

    Made a LFSR for Project 2

---
+++ Project2/Code/LFSR.v
@@ -0,0 +1,17 @@
+module LFSR #(parameter SIZE = 1)(clk, value);
+	input clk;
+	output reg [SIZE-1:0] value;
+
+	reg feedback;
+	
+	always@(posedge clk)
+	begin
+		if(value == {(SIZE-1){1'b0}})
+		begin
+			value = {(SIZE-1){1'b1}};
+		end
+		feedback = value[SIZE-1] ^ value[SIZE-2];
+		value = value << 1;
+		value[0] = feedback;
+	end
+endmodule
--- Project2/Code/top.v
+++ Project2/Code/top.v
@@ -3,6 +3,20 @@ module top(SW, LEDR, ADC_CLK_10);
 	output[9:0] LEDR;
 	input ADC_CLK_10;
 	
-	clk_divider #(14) cd(ADC_CLK_10, 14'h2710, LEDR); // Magic value = 10,000	
+	wire slow_clk;
+	wire [6:0] rand_num; 
+	reg [6:0] displayed_num;
+	
+	clk_divider #(24) cd(ADC_CLK_10, 24'd10000000, slow_clk); // Magic value = 1s
+	LFSR #(7) lin(ADC_CLK_10, rand_num);
+	
+	always@(posedge slow_clk)
+	begin
+		displayed_num = rand_num;
+	end
+	
+	assign LEDR[6:0] = displayed_num;
+	assign LEDR[8:7] = SW[8:7];
+	assign LEDR[9] = slow_clk;
 	
 endmodule
