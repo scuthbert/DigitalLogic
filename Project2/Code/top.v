7fb2e20f8cfe41192c7b71b19b047ef0a3e70a70
Sam Cuthbertson <samuel.cuthbertson@colorado.edu>
2017-04-17 17:01:38 -0600

    Added commenting, removed shift reg

--- Project2/Code/top.v
+++ Project2/Code/top.v
@@ -26,7 +26,7 @@ module top(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ADC_CLK_10);
 	reg [24:0] display;
 	BCD_decoder BD(decimal, display, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5); // Display the contents of reg display
 	
-	//GO BUFFS slider
+	// GO BUFFS slider
 	reg [24:0] go_buffs;
 	reg [3:0] position;
 	always@(posedge super_slow_clk)
@@ -34,7 +34,7 @@ module top(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ADC_CLK_10);
 	
 		if(go_buffs == 24'd0)
 		begin
-			go_buffs = 24'b110111011101110111011101; //Blank
+			go_buffs = 24'b110111011101110111011101; // Blank
 		end
 		
 		go_buffs = go_buffs << 4;
@@ -45,12 +45,12 @@ module top(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ADC_CLK_10);
 		case(position)
 			4'd0: go_buffs[3:0] = 4'h6;
 			4'd1: go_buffs[3:0] = 4'h0;
-			4'd2: go_buffs[3:0] = 4'hd;//Space
+			4'd2: go_buffs[3:0] = 4'hd;// Space
 			4'd3: go_buffs[3:0] = 4'hb;
-			4'd4: go_buffs[3:0] = 4'hc;//U (obvi)
+			4'd4: go_buffs[3:0] = 4'hc;// U (obvi)
 			4'd5: go_buffs[3:0] = 4'hf;
 			4'd6: go_buffs[3:0] = 4'hf;
-			4'd7: go_buffs[3:0] = 4'h5;//S (also obvi)
+			4'd7: go_buffs[3:0] = 4'h5;
 			4'd8: go_buffs[3:0] = 4'hd;
 			4'd9: go_buffs[3:0] = 4'hd;
 			4'd10: go_buffs[3:0] = 4'hd;
@@ -60,77 +60,77 @@ module top(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ADC_CLK_10);
 
 	end
 	
-	//Main state machine
+	// Main state machine variables
 	reg [1:0] state;
 	reg [3:0] switch_num;
 	reg [19:0] stored_time;
 	
-	always@(posedge ADC_CLK_10, negedge KEY[0]) //Update super quick
+	always@(posedge ADC_CLK_10, negedge KEY[0]) // Update super quick, or on reset
 	begin
 	
-		if(~KEY[0])
+		if(~KEY[0]) //Reset
 		begin
-			clreset = 1'b1;
-			LEDR = 10'd0;
-			state = 2'b00;
+			clreset = 1'b1; // Set the clock to reset
+			LEDR = 10'd0; // Clear the LEDs
+			state = 2'b00; // Set the state to PS
 		end
 		
 		else
 		begin
 			case(state)
-			2'b00: //PS, aka 'GO BUFFS'
+			2'b00: // PS, aka 'GO BUFFS'
 				begin
 				
-					decimal = 1'b0;
-					display = go_buffs; //Blank
+					decimal = 1'b0; // Blank
+					display = go_buffs; // Display contents of go_buffs
 						
-					if(~KEY[1])
+					if(~KEY[1]) // If start
 					begin
-						rand_num_saved = rand_num;
-						clreset = 1'b1;
-						state = 2'b01; //RW
+						rand_num_saved = rand_num; // Save a random number
+						clreset = 1'b1; // Reset the clock
+						state = 2'b01; // RW
 					end
 					
 				end
-			2'b01: //RW, wait until counter = stored rand
+			2'b01: // RW, wait until counter = stored rand
 				begin
 					
-					if(~KEY[1])
+					if(~KEY[1]) // If start is pressed
 					begin
-						clreset = 1'b1;
+						clreset = 1'b1; // Reset our clock
 					end
-					else if(clreset == 1) //If the counter is being reset
+					else if(clreset == 1) // Else, if the counter is being reset
 					begin
-						clreset = 1'b0; //Enable the counter
+						clreset = 1'b0; // Enable the counter
 					end
 					
-					decimal = 1'b0;
-					display = 24'b110111011101110111011101; //Blank
+					decimal = 1'b0; // Blank
+					display = 24'b110111011101110111011101; // Blank
 					
-					if(rand_num_saved == count_binary)
+					if(rand_num_saved == count_binary) // Our exit scenario
 					begin
-						switch_num = count[3:0];
-						clreset = 1'b1; //Reset the counter
-						state = 2'b10; //Wait on the user
+						switch_num = count[3:0]; // Save a switch to use
+						clreset = 1'b1; // Reset the counter
+						state = 2'b10; // Wait on the user
 					end
 					
 				end
-			2'b10: //UW, count time spent here
+			2'b10: // UW, count time spent here
 				begin
 				
-					if(clreset == 1'b1)
+					if(clreset == 1'b1) // If our counter is being reset
 					begin
-						clreset = 1'b0;
+						clreset = 1'b0; // Enable the counter
 					end
 					
-					decimal = 1'b0;
-					display = 24'b110111011101110111011101; //Blank
+					decimal = 1'b0; // Blank
+					display = 24'b110111011101110111011101; // Blank
 					
-					case(switch_num)
+					case(switch_num) // Check the correct switch. If it's on, change state.
 						4'b0000: 
 						begin
-							LEDR = 10'b0000000001;
-							if(SW[0] == 1) state = 2'b11;
+							LEDR = 10'b0000000001; // Turn on the right LED.
+							if(SW[0] == 1) state = 2'b11; // If SW[correct], go to display.
 						end
 						4'b0001: 
 						begin
@@ -182,25 +182,24 @@ module top(SW, KEY, LEDR, HEX0, HEX1, HEX2, HEX3, HEX4, HEX5, ADC_CLK_10);
 				end
 			2'b11: //DS, display the count from UW
 				begin
-					if(clreset == 1'b0)
+					if(clreset == 1'b0) // If our counter is running
 					begin
-						stored_time = count;
-						clreset = 1'b1;
+						stored_time = count; // Save the current count
+						clreset = 1'b1; // Set the counter to reset
 					end
 					
-					LEDR = 10'b0000000000;
-					decimal = 1'b1;
-					display = stored_time;
+					LEDR = 10'b0000000000; // Clear the LEDs 
+					decimal = 1'b1; // Display a decimal point
+					display = stored_time; // Display the user's time
 					
-					if(~KEY[1])
+					if(~KEY[1]) // If start is pressed
 					begin
-						clreset = 1'b1;
-						state = 2'b01;
+						clreset = 1'b1; // Reset the clock
+						state = 2'b01; // Set the state to RW
 					end
 					
 				end
 			endcase
 		end
 	end
-	
 endmodule
